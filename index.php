43ab0e4c4abb801e7dc8ca6ace9cda3b0f1e3064
Bas van Berckel <bvanberckel@student.uva.nl>
2015-01-07 14:56:28 +0100

    Commit 2

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <!DOCTYPE HTML>
+<html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
   <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
   <?php
-    include('header.php'); 
-    $page = $_GET['page'].'.php'
+    include('header.php');
+     
+    if isset($_GET['page']) {
+      $page = $_GET['page'].'.php';
+    } else {
+      $page = 'main.php';
+    }
+    
     if (is_file($page) && is_readable($page)) {
       include($page);
     } else {
-      include('404.php')
+      include('404.php');
     }
-    include('footer.php')
+    
+    include('footer.php');
   ?>
-  
+</body>
+</html>
   
---
+++ topic.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+  <link rel="stylesheet" type="text/css" href="topic.css">
+</head>
+<body>
+  <div id="thread">
+    <table>
+    <div class="post">
+      <div class="post-content">
+      </div>
+      <div class="post-data">
+      </div>
+    </div>
+  </div>
+</body>
+</html>
