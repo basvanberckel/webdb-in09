ca602f8dac2309e3df84774aeab705197557c09f
Bas van Berckel <bvanberckel@student.uva.nl>
2015-01-08 14:42:16 +0100

    fixed merge

--- footer.php
+++ footer.php
@@ -1,12 +1,6 @@
 
 <div id="footer">
-<<<<<<< HEAD
 <div class="link"><a href="#">Link 1</a></div>
 <div class="link"><a href="#">Link 2</a></div>
 <div class="link"><a href="#">en nog een link..</a></div>
-=======
-<span class="link"><a href="#">Link 1</a></span>
-<span class="link"><a href="#">Link 2</a></span>
-<span class="link"><a href="#">en nog een link..</a></span>
->>>>>>> 200b14e66ae8239789fc645d69eeeae6cceace55
 </div>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+<!DOCTYPE HTML>
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
@@ -23,32 +23,6 @@
     require($page.'.php');
     echo "</div>\n";    
     require('footer.php');
-=======
-<!DOCTYPE HTML>
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-  <link rel="stylesheet" type="text/css" href="main.css">
-</head>
-<body>
-  <?php
-    include('header.php');
-     
-    if (isset($_GET['page'])) {
-      $page = $_GET['page'].'.php';
-    } else {
-      $page = 'main.php';
-    }
-    
-    if (is_file($page) && is_readable($page)) {
-      include($page);
-    } else {
-      include('404.php');
-    }
-    
-    include('footer.php');
->>>>>>> 200b14e66ae8239789fc645d69eeeae6cceace55
-  ?>
 </body>
 </html>
   
