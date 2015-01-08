c834df90a6a4a35006cd64a14c8401f83f5462f4
Rosco Kalis <roscokalis@gmail.com>
2015-01-08 11:42:38 +0100

    Added Admin panel html

---
+++ admin/moderation.php
@@ -0,0 +1,7 @@
+These messages require your approval:
+<form action="process_post.php">
+	<div class="post">This is a post</div>
+	<input type="hidden" name="post-id" value="5">
+	<input type="submit" name="submit" id="approve" value="Approve">
+	<input type="submit" name="submit" id="decline" value="Decline">
+</form>
