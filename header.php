09145b5365331f44efb9b25673670b99184bdb48
Casimir Geelhoed <casimirgeelhoed@gmail.com>
2015-01-14 15:15:02 +0100

    Login beginnetje

--- header.php
+++ header.php
@@ -5,11 +5,7 @@
 <a href="/" alt="The Duke's Herald"><img src="images/logo.png"></a>
 </div>
 
-<div id="login">
-	Hier komt de login.. <br />
-<input type="text" name="username" /> <br />
-<input type="password" name="password" /> <br />
-<a href="#">Wachtwoord vergeten?</a>
-</div>
+<?php require('login.php') ?>
+
 
 </div>
