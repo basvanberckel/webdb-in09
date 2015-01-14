1a331c59710a60192ad79d42f616b2c17e19aef2
Amos Bastian <amos_bastian@hotmail.com>
2015-01-14 12:31:04 +0100

    link to index.php now working

--- information.php
+++ information.php
@@ -3,6 +3,6 @@
 	<p>Thank you for registering. A confirmation email has been sent to "insert email here". If you haven't received an email within a few seconds, then please check your spam folder. Otherwise contact the admin for further assistance.
 	<br />
 	<br />
-	<a href="#">Click here to return to  the home page.</a>
+	<a href="index.php">Click here to return to  the home page.</a>
 	</p>
 </div>
