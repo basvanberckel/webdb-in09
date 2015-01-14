e59777844f24edf11a1de94c38a0016dbe9ec608
Bas van Berckel <bas.vanberckel@student.uva.nl>
2015-01-14 14:55:51 +0100

    cat fix

--- main.php
+++ main.php
@@ -5,11 +5,12 @@
     $res->data_seek($row_no);
     $row = $res->fetch_assoc();
     $title = $row['title'];
+    $cid = $row['cid'];
     echo "
     <div class='category'>
       <h2>$title</h2> <hr />
     ";
-    $fres = dbquery("SELECT * FROM forums WHERE parent_id=$row_no AND main=1 ORDER BY position;");
+    $fres = dbquery("SELECT * FROM forums WHERE parent_id=$cid AND main=1 ORDER BY position;");
     for ($frow_no = 0; $frow_no < $fres->num_rows; $frow_no++) {
       $fres->data_seek($frow_no);
       $frow = $fres->fetch_assoc();
