4f8e4bb4a28a62f733d586399a406403cbe3f121
Jason Compier <jason.compier@student.uva.nl>
2015-01-19 14:28:38 +0100

    More database testing

--- discussion.php
+++ discussion.php
@@ -1,9 +1,8 @@
 <?php
     dbconnect();
-    $res = dbquery("INSERT INTO `forum`.`posts` (title, comment)
-                    VALUES (:title :comment)",
-                    array('title'=>$_POST['title'],
-                        'comment'=>$_POST['content']));
+    $res = dbquery("INSERT INTO posts (title, comment)
+                    VALUES ('dbtest', 'blablabla')");
+                   
 
 ?>
 <h1>New Discussion</h1>
@@ -12,8 +11,8 @@
 <form method="post">
 <fieldset>
              
-    <div><label for="title">Discussion Title:</label></div> <input type="text" name="title"><br>
-    <div><label for="comment">Comment:</label></div> <textarea type="text" name="comment" cols=50 rows=10></textarea>
+    <div>Discussion Title:</div> <input type="text" name="title"><br>
+    <div>Comment:</div>div> <textarea type="text" name="comment" cols=50 rows=10></textarea>
     <div><input type="submit" value="Post Discussion"></div>
 	  			
 </fieldset>
