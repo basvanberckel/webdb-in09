71bd26688840f7f63a51411af1ad133007bc5b54
Jason Compier <jason.compier@student.uva.nl>
2015-01-19 14:14:44 +0100

    More database testing

--- discussion.php
+++ discussion.php
@@ -12,8 +12,8 @@
 <form method="post">
 <fieldset>
              
-    <div>Discussion Title:</div> <input type="text" name="title"><br>
-    <div>Comment:</div> <textarea type="text" name="comment" cols=50 rows=10></textarea>
+    <div><label for="title">Discussion Title:</label></div> <input type="text" name="title"><br>
+    <div><label for="comment">Comment:</label></div> <textarea type="text" name="comment" cols=50 rows=10></textarea>
     <div><input type="submit" value="Post Discussion"></div>
 	  			
 </fieldset>
