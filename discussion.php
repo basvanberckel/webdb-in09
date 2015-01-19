2b7054adc8e6b973722ca7329052c713e4123733
Jason Compier <jason.compier@student.uva.nl>
2015-01-19 15:19:29 +0100

    More database testing

--- discussion.php
+++ discussion.php
@@ -1,10 +1,13 @@
 <?php
 if ($_POST) {
     dbconnect();
+    $title = $_POST['title'];
+    $content = $_POST['content'];
     $res = dbquery("INSERT INTO posts (pid, uid, title, content, date, tid, approved)
                     VALUES ( , , 'dbtest', 'blablabla', CURRENT_TIMESTAMP, , )");
 }
-
+if ($res) {
+    print "Post succesful!";
 ?>
 <h1>New Discussion</h1>
 
@@ -13,7 +16,7 @@ if ($_POST) {
 <fieldset>
              
     <div>Discussion Title:</div> <input type="text" name="title"><br>
-    <div>Comment:</div> <textarea type="text" name="comment" cols=50 rows=10></textarea>
+    <div>Comment:</div> <textarea type="text" name="content" cols=50 rows=10></textarea>
     <div><input type="submit" value="Post Discussion"></div>
 	  			
 </fieldset>
