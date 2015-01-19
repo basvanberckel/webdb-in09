5acb494dd8e98eab40f524aeaa6205233c470398
Rosco Kalis <roscokalis@gmail.com>
2015-01-19 19:16:39 +0100

    Added the PHP for the forum management. Works for main forums, still need to implement for subforums. Forum creation and moderation queue aren't working yet.

--- admin/creation.php
+++ admin/creation.php
@@ -1,7 +1,19 @@
 <div class="content" id="creation-form">
+
+	<?php
+		if(isset($_POST['forum-title']) && isset($_POST['forum-desc'])
+		&& isset($_POST['forum-mod'])   && isset($_POST['forum-cat']) 
+		&& isset($_POST['forum-pos'])) {
+			$title = $_POST['forum-title']
+			$desc = $_POST['forum-desc']
+			$mod = $_POST['forum-mod']
+			$cat = $_POST['forum-cat']
+			$pos = $_POST['forum-pos']
+		}
+	?>
 	Create a new forum:
 	<br /><br />
-	<form action="create_forum.php">
+	<form method="post">
 		<div class="form-header">Forum title:</div>
 		<input type="text" name="forum-title" id="forum-title">
 		<div class="form-header">Forum description:</div>
@@ -16,6 +28,5 @@
 		<div class="buttons">
 			<button type="submit" name="submit" id="submit">Sumbit</button>
 		</div>
-		
 	</form>
 </div>
--- admin/index.php
+++ admin/index.php
@@ -2,8 +2,11 @@
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+  <link rel="stylesheet" type="text/css" href="../styles/all.css">
+  <link rel="stylesheet" type="text/css" href="../styles/main.css">
   <link rel="stylesheet" type="text/css" href="../styles/admin.css">
   <?php
+	require('../dbconfig.php');
     if (isset($_GET['page'])) {
       $page = $_GET['page'];
     } else {
@@ -20,6 +23,6 @@
 	require('header.php');
     require($page.'.php');
     echo '<br /></div>';    
-  ?>
+   ?>
 </body>
 </html>
