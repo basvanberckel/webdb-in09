5cfae27ec0b542d514a2dc1e587d00c98c65779c
Jason Compier <jason.compier@student.uva.nl>
2015-01-19 15:12:17 +0100

    More database testing

--- discussion.php
+++ discussion.php
@@ -1,14 +1,15 @@
 <?php
+if ($_POST) {
     dbconnect();
-    $res = dbquery("INSERT INTO posts (title, comment)
-                    VALUES ('dbtest', 'blablabla')");
-                   
+    $res = dbquery("INSERT INTO posts (pid, uid, title, content, date, tid, approved)
+                    VALUES ( , , 'dbtest', 'blablabla', CURRENT_TIMESTAMP, , )");
+}
 
 ?>
 <h1>New Discussion</h1>
 
 <!-- Temporary link to profile, should link to thread page -->
-<form method="post">
+<form method="POST">
 <fieldset>
              
     <div>Discussion Title:</div> <input type="text" name="title"><br>
