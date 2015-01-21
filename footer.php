56b5f7940459440a90dc9da6c4a3d39fef08fd7d
Casimir Geelhoed <casimirgeelhoed@gmail.com>
2015-01-21 14:16:53 +0100

    Footer.

--- footer.php
+++ footer.php
@@ -1,6 +1,12 @@
 
 <div id="footer">
-<span class="link"><a href="#">Link 1</a></span>
-<span class="link"><a href="#">Link 2</a></span>
-<span class="link"><a href="#">en nog een link..</a></span>
+
+<span class="link"><a href="index.php">Home</a></span>
+
+<?php if(array_key_exists('user', $_SESSION) && $_SESSION['user']->role == 10){ ?>
+<span class="link"><a href="/admin">Admin panel</a></span>
+<?php } ?>
+
+<span class="link"><a href="#">About</a></span>
+<span class="link"><a href="#">Contact</a></span>
 </div>
