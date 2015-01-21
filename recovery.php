ab7a4bbe9beffc0252218e578227ab503004bd59
Amos Bastian <amos_bastian@hotmail.com>
2015-01-21 11:53:31 +0100

    password recovery now working

--- recovery.php
+++ recovery.php
@@ -2,15 +2,20 @@
 if(isset($_POST['email'])) {
     dbconnect();
     $email=$_POST['email']; 
-    $res = dbquery("SELECT username, passwd FROM users 
+    $res = dbquery("SELECT username FROM users 
                   WHERE email='$email';");
     while($row = $res->fetch(PDO::FETCH_ASSOC)) {
         $username = $row['username'];
-        $passwd = $row['passwd'];
     }
 
     if($_POST['email']) {
-    echo $username . $passwd;
+        
+    function newPassword($length) {
+        $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%*()_+{}|<>?/.,;[]\=-";
+        return substr(str_shuffle($characters), 0, $length);
+    }
+    $password = newPassword(10);
+    echo $password;
     $to = $email;
     $subject = "Duke's Herald - Account Recovery";
     $headers = "MIME-Version: 1.0" . "\r\n";
@@ -29,7 +34,7 @@ if(isset($_POST['email'])) {
     <div>
     <p>Hello  $username, someone has requested the login info of your Duke's Herald account. <br /> <br />
     Username: $username
-    Password: $passwd</p>
+    Password: $password</p>
     </div>
     </div>
     </body>
@@ -37,6 +42,8 @@ if(isset($_POST['email'])) {
     ";
 
     mail($to, $subject, $message, $headers);
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+    $res = dbquery("UPDATE users SET passwd='$hash' WHERE email='$email'");
     }
 }
 else { ?>
@@ -45,7 +52,7 @@ else { ?>
     <form method="POST">
         <fieldset>
             <legend>Account Recovery</legend>
-            <p>If you have forgotten your username or password, you can enter the e-mail address you used to sign up with below, and we'll then send you your login info. <br /> <br />
+            <p>If you have forgotten your username or password, you can enter the e-mail address you used to sign up with below, and we'll then send you your username and a new password. <br /> <br />
                 If you also can't remember the e-mail address you used to sign up with, then there isn't anything we can do, unfortunately - but account registration is free, so if you want, you can always create a new account instead. <br /><br /></p>
             
             <div>
