2c47171f19650d5a46204dae4aecb55302230ea9
Jason Compier <jason.compier@student.uva.nl>
2015-01-22 14:21:22 +0100

    IF testing

--- profile.php
+++ profile.php
@@ -1,7 +1,7 @@
 <h1>Account Details</h1>
 <?php
     dbconnect();
-    $res = dbquery("SELECT * FROM users");
+    $res = dbquery("SELECT * FROM users WHERE uid = $_GET['uid']");
     
 
     while($row = $res->fetch(PDO::FETCH_ASSOC)) {
@@ -15,65 +15,65 @@
     
     
         if ($_SESSION['user']->uid == $_GET['uid']) {
-            echo "Dit is een test bericht";
-        }
-    
+        
+            echo "
+                <div id='profile'>
+                <form method='POST'>
+                    <fieldset>
+                        <legend>Credentials</legend>
+
+                        <div>
+                            <label for='avatar'><b>Avatar:</b></label>
+
+                        </div>
+
+                        <div>
+                            <label for='username'><b>Username:</b></label>
+                            <input type='text' name='username' id='username' class='txt' value=$username/>
+                        </div>
+
+                        <div>
+                            <label for='email'><b>E-mail address:</b></label>
+                            <input type='text' name='email' id='email' class='txt' value=$email/>
+                        </div>
+
+                        <div>
+                            <label for='password'><b>Password:</b></label>
+                            <input type='password' name='password' id='password' class='txt' value=$passwd/>
+                        </div>
+
+                        <div>
+                            <label for='dob'><b>Date of birth:</b></label>
+                            <input type='date' name='dob' id='dob' max='2015-01-31' min='1900-01-01' class='txt' value=$dob/>
+                        </div>
+
+                        <div>
+                            <label for='sex'><b>Sex:</b></label>
+                            <input type='radio' name='sex' value='man' checked>Male
+                            <input type='radio' name='sex' value='vrouw' >Female
+                        </div> 
+                    </fieldset>
+            </div>
+
+            <div id='profile'>
+                <fieldset>
+                    <legend>Settings</legend>
+                    <div>
+                        <label for='bio'><b>Bio:</b></label>
+                        <textarea type='text' name='bio' id='bio' class='txt' value=$bio></textarea>
+                    </div>
+                </fieldset>
+
+                <div class='center'>
+                    <div class='buttons'>
+                        <button type='submit' class='small' value='Edit'>Edit</button>
+                        <button type='reset' class='small' value='Save'>Save</button>
+                    </div>
+                </div>
+                </form>
+            </div>
+        "
     }
         
     
 ?>
-<div id="profile">
-	<form method="POST">
-		<fieldset>
-			<legend>Credentials</legend>
-            
-            <div>
-				<label for="avatar"><b>Avatar:</b></label>
-                
-            </div>
-            
-			<div>
-				<label for="username"><b>Username:</b></label>
-				<input type="text" name="username" id="username" class="txt" value="poep"/>
-			</div>
-
-			<div>
-				<label for="email"><b>E-mail address:</b></label>
-				<input type="text" name="email" id="email" class="txt"/>
-			</div>
-
-			<div>
-				<label for="password"><b>Password:</b></label>
-				<input type="password" name="password" id="password" class="txt"/>
-			</div>
-                
-            <div>
-                <label for="dob"><b>Date of birth:</b></label>
-                <input type="date" name="dob" id="dob" max="2015-01-31" min="1900-01-01" class="txt"/>
-            </div>
-
-            <div>
-                <label for="sex"><b>Sex:</b></label>
-				<input type="radio" name="sex" value="man" checked>Male
-				<input type="radio" name="sex" value="vrouw" >Female
-            </div> 
-		</fieldset>
-</div>
-    
-<div id="profile">
-    <fieldset>
-        <legend>Settings</legend>
-        <div>
-            <label for="bio"><b>Bio:</b></label>
-            <textarea type="text" name="bio" id="bio" class="txt"></textarea>
-        </div>
-    </fieldset>
-    
-	<div class="center">
-		<div class="buttons">
-			<button type="submit" class="small" value="Edit">Edit</button>
-			<button type="reset" class="small" value="Save">Save</button>
-		</div>
-	</div>
-	</form>
-</div>
