a02f62e765d3e09a3372e026d96d6bee0f8d2b13
Amos Bastian <amos_bastian@hotmail.com>
2015-01-22 20:55:40 +0100

    checks now working (hopefully), database check still needs to be added

--- registration.php
+++ registration.php
@@ -1,23 +1,98 @@
 <?php
-if(isset($_POST['username'])) {
-    dbconnect();
-    $passwd = $_POST['password'];
-    $dob = $_POST['dob'];
-    $sex = $_POST['sex'];
-    $hash = password_hash($passwd, PASSWORD_DEFAULT);
-    $email = $_POST['email'];
-    $username = $_POST['username'];
-    $res = dbquery("INSERT INTO users (username, email, passwd, dob, sex)
+function test_input($data) {
+    $data = trim($data, "\t\n\r\0\x0B");
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+function checkError($error) {
+    foreach (func_get_args() as $error) {
+        if (empty($error))
+            continue;
+        else {
+            return false;
+        }          
+    }
+    return true;
+}
+
+$username = $email = $passwd = $dob = $sex = "";
+$usernameError = $emailError = $passwordError = $dobError = $sexError = "";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    /* TEST USERNAME */
+    if (empty($_POST['username'])) {
+        $usernameError = "A username is required.";   
+    }
+    elseif (strlen($_POST['username']) < 6) {
+        $usernameError = "The username you entered is too short.";
+    }   
+    elseif (strlen($_POST['username']) > 24) {
+        $usernameError = "The username you entered is too long.";   
+    }
+    elseif ((preg_match("/^[a-zA-Z0-9]+$/", $_POST['username'])) == 0) {
+        $usernameError = "The username you entered contains illegal characters.";   
+    }
+    else {
+        $username = $_POST['username'];   
+    }
+    /* ^NEED TO ADD CHECK FOR DATABASE*/
+    
+    /* TEST E-MAIL ADDRESS */
+    if (empty($_POST['email'])) {
+        $emailError = "An e-mail address is required.";   
+    } 
+    elseif ($_POST['email'] != $_POST['email_confirmation']) {
+        $passwordError = "The e-mails you entered do not match each other.";   
+    }
+    else {
+        $email = $_POST['email'];   
+    }
+    /* ^NEED TO ADD CHECK FOR DATABASE*/
+    
+    /* TEST PASSWORD */
+    if (empty($_POST['password'])) {
+        $passwordError = "A password is required.";
+    }
+    elseif (strlen($_POST['password']) < 6) {
+        $passwordError = "The password you entered is too short.";   
+    }
+    elseif (strlen($_POST['password']) > 40) {
+        $passwordError = "The password you entered is too long.";   
+    }
+    elseif ($_POST['password'] != $_POST['password_confirmation']) {
+        $passwordError = "The passwords you entered do not match each other.";   
+    }
+    else {
+        $passwd = $_POST['password'];   
+    }
+    
+    /* TEST DATE OF BIRTH */
+    if (empty($_POST['dob'])) {
+        $dobError = "A date of birth is required.";   
+    }
+    else {
+        $dob = $_POST['dob'];   
+    }
+    
+    if (empty($_POST['sex'])) {
+        $sexError = "A gender is required";   
+    }
+    else {
+        $sex = $_POST['sex'];   
+    }
+    
+    if (checkError($usernameError, $emailError, $passwordError, $dobError, $sexError)) {
+        dbconnect();
+        $hash = password_hash($passwd, PASSWORD_DEFAULT);
+        $res = dbquery("INSERT INTO users (username, email, passwd, dob, sex)
         VALUES (:username, :email, :passwd, :dob, :sex);",
         array('username'=>$_POST['username'],
                  'email'=>$_POST['email'],
                 'passwd'=>$hash,
                    'dob'=>$_POST['dob'],
                    'sex'=>$_POST['sex']));
-    
-    $userLength = strlen($username);
-    $pswdLength = strlen($passwd);
-    
     
     if($res) {
         echo "Thank you for registering. A verification e-mail has been sent to " . $email . ". If you haven't received an email within a few seconds, then please check your spam folder. Otherwise contact the admin for further assistance.";
@@ -50,56 +125,58 @@ if(isset($_POST['username'])) {
     else {
         echo "Registration has failed, please try again.";
     }  
+    }
 } 
-else { ?>
+?>
+
 
 <div id="registration">
-	<form method="POST">
+	<form method="POST" action="index.php?page=registration">
 		<fieldset>
 			<legend>Registration</legend>
 			<div>
 				<label for="username"><b>Username:</b></label>
-				<input type="text" name="username" id="username" class="txt" required/>
-                <span class="error"> * </span> <span class="required">* = required</span>
+				<input type="text" name="username" id="username" class="txt"/>
+                <span class="error"> * <?php echo $usernameError;?></span>
                 <br>
                 <span class="correct">Must be between 6 <br /> and 24 characters.</span>
 			</div>
 
 			<div>
 				<label for="email"><b>E-mail address:</b></label>
-				<input type="text" name="email" id="email" class="txt" required/>
-                <span class="error"> * </span>
+				<input type="text" name="email" id="email" class="txt"/>
+                <span class="error"> * <?php echo $emailError;?></span>
 			</div>
 
 			<div>
 				<label for="email_confirmation"><b>Confirm your e-mail address:</b></label>
-				<input type="text" name="email_confirmation" id="email_confirmation" class="txt" required/>
-                <span class="error"> * </span>
+				<input type="text" name="email_confirmation" id="email_confirmation" class="txt"/>
 			</div>
 
 			<div>
 				<label for="password"><b>Password:</b></label>
-				<input type="password" name="password" id="password" class="txt" required/>
-                <span class="error"> * </span>
+				<input type="password" name="password" id="password" class="txt"/>
+                <span class="error"> * <?php echo $passwordError;?></span>
                 <br>
                 <span class="correct">Must be between 6 <br /> and 40 characters.</span>
 			</div>
 
 			<div>
 				<label for="password_confirmation"><b>Confirm your password:</b></label>
-				<input type="password" name="password_confirmation" id="password_confirmation" class="txt" required/>
-                <span class="error"> * </span>
+				<input type="password" name="password_confirmation" id="password_confirmation" class="txt"/>
 			</div>
                 
             <div>
                 <label for="dob"><b>Date of birth:</b></label>
                 <input type="date" name="dob" id="dob" max="2015-01-31" min="1900-01-01" class="txt"/>
+                <span class="error"> * <?php echo $dobError;?></span>
             </div>
 
             <div>
-                <label for="sex"><b>Sex:</b></label>
+                <label for="sex"><b>Gender:</b></label>
 				<input type="radio" name="sex" value="man" >Male
 				<input type="radio" name="sex" value="vrouw" >Female
+                <span class="error"> * <?php echo $sexError;?></span>
             </div> 
 		</fieldset>
 
@@ -117,4 +194,3 @@ else { ?>
 	</div>
 	</form>
 </div>
-<?php } ?>
