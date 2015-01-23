87cc5b50ee5c62078d17088e6b4f1c01a6a1f085
Jason Compier <jason.compier@student.uva.nl>
2015-01-23 14:33:00 +0100

    Button testing

--- profile.php
+++ profile.php
@@ -32,32 +32,32 @@
                         <div>
                             <label for='email'><b>E-mail address:</b></label>
                             <input type='text' name='email' id='email' class='txt' readonly='readonly' value=$email />
-                            <button type='button' onclick='editEmail()'  value='edit'>Edit</button>
+                            <button id='emailb' type='button' onclick='editEmail()'  value='edit'>Edit</button>
                         </div>
 
                         <div>
                             <label for='password'><b>Password:</b></label>
+                            <a href='thing'>Change password</a>
                         </div>
                         
                         <div>
                             <label for='dob'><b>Date of birth:</b></label>
                             <input type='date' name='dob' id='dob' max='2015-01-31' min='1900-01-01' readonly='readonly' class='txt' value=$dob />
-                            <button type='button' onclick='editDob()' value='edit'>Edit</button>
+                            <button id='dobb' type='button' onclick='editDob()' value='edit'>Edit</button>
 
                         </div>
 
                         <div>
                             <label for='sex'><b>Sex:</b></label>
-                            <input type='radio' name='sex' value='man' checked>Male
-                            <input type='radio' name='sex' value='vrouw' >Female
-                            <button type='button' onclick='editSex()' value='edit'>Edit</button>
+                            if ($sex == m) {echo 'Male'}
+                            else {echo 'Female'}
 
                         </div> 
 
                         <div>
                             <label for='bio'><b>Bio:</b></label>
                             <textarea type='text' name='bio' id='bio' readonly='readonly' class='txt'>$bio</textarea>
-                            <button type='button' onclick='editBio()' value='edit'>Edit</button>
+                            <button id='biob' type='button' onclick='editBio()' value='edit'>Edit</button>
 
                         </div>
                     
@@ -97,19 +97,38 @@
 <script>
 
     function editEmail() {
-
+        var elem = document.getElementById("emailb");
+        if (elem.value=="Edit") { 
+            elem.removeAttribute('readonly'); 
+            elem.value = "Save";
+        }
+        else {
+            elem.setAttribute("readonly", "readonly")
+            elem.value = "Edit";
+        }
     }
 
     function editDob() {
-
-    }
-
-    function editSex() {
-
+        var elem = document.getElementById("dobb");
+        if (elem.value=="Edit") { 
+            elem.removeAttribute('readonly'); 
+            elem.value = "Save";
+        }
+        else {
+            elem.setAttribute("readonly", "readonly")
+            elem.value = "Edit";
+        }
     }
 
     function editBio() {
-
+        var elem = document.getElementById("biob");
+        if (elem.value=="Edit") { 
+            elem.removeAttribute('readonly'); 
+            elem.value = "Save";
+        }
+        else {
+            elem.setAttribute("readonly", "readonly")
+            elem.value = "Edit";
+        }
     }
-    
 </script>
