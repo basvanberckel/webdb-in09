0d58691cbe63811bb0327a7aec4aed31027d1060
Jason Compier <jason.compier@student.uva.nl>
2015-01-23 14:05:53 +0100

    Button testing

--- profile.php
+++ profile.php
@@ -32,7 +32,7 @@
                         <div>
                             <label for='email'><b>E-mail address:</b></label>
                             <input type='text' name='email' id='email' class='txt' readonly='readonly' value=$email />
-                            <div class='buttons inline'><button type='button' onclick='editEmail()'  value='edit'>Edit</button></div>
+                            <button type='button' onclick='editEmail()'  value='edit'>Edit</button>
                         </div>
 
                         <div>
@@ -42,7 +42,7 @@
                         <div>
                             <label for='dob'><b>Date of birth:</b></label>
                             <input type='date' name='dob' id='dob' max='2015-01-31' min='1900-01-01' readonly='readonly' class='txt' value=$dob />
-                            <div class='buttons inline'><button type='button' onclick='editDob()' value='edit'>Edit</button></div>
+                            <button type='button' onclick='editDob()' value='edit'>Edit</button>
 
                         </div>
 
@@ -50,14 +50,14 @@
                             <label for='sex'><b>Sex:</b></label>
                             <input type='radio' name='sex' value='man' checked>Male
                             <input type='radio' name='sex' value='vrouw' >Female
-                            <div class='buttons inline'><button type='button' onclick='editSex()' value='edit'>Edit</button></div>
+                            <button type='button' onclick='editSex()' value='edit'>Edit</button>
 
                         </div> 
 
                         <div>
                             <label for='bio'><b>Bio:</b></label>
                             <textarea type='text' name='bio' id='bio' readonly='readonly' class='txt'>$bio</textarea>
-                            <div class='buttons inline'><button type='button' onclick='editBio()' value='edit'>Edit</button></div>>
+                            <button type='button' onclick='editBio()' value='edit'>Edit</button>
 
                         </div>
                     
