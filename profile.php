6b7d193dd491a514a3bd0ab6e944deb2483595aa
Jason Compier <jason.compier@student.uva.nl>
2015-01-27 14:48:40 +0100

    Edit/Save button

--- profile.php
+++ profile.php
@@ -98,7 +98,7 @@
         if (elem.innerHTML == "edit") { 
             document.getElementById("email").disabled = false; 
             elem.innerHTML = "save";
-            <?php $email = $_POST['email'];
+            <?php
             $res2 = dbquery("UPDATE users SET email = :email WHERE uid = :uid",
                     array('uid' => $uid,
                           'email' => $email));
