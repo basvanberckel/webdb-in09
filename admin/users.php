5454c348c87e3e941f4556d6f14bb215007b1ec3
Rosco Kalis <roscokalis@gmail.com>
2015-01-28 17:40:29 +0100

    Created a user management page, functionality not yet working.

--- admin/users.php
+++ admin/users.php
@@ -1,16 +1,26 @@
 <div class="content" id="users">
 
 <?php
-$res = dbquery("SELECT * FROM users WHERE 1 ORDER BY username");
+$res = dbquery("SELECT *
+				FROM users 
+				ORDER BY username");
 while ($user = $res->fetchObject()) {
-  echo "<a href='/?page=profile?uid={$user->uid}'>
-          <div class='user'>
-            <p>{$user->username}</p>
-            <form>
-              <input type='checkbox' name='verified' />
-              <input type='text' name='role' cols='2' />
-            </form>
-          </div>
-        </a>";
+	$uid = $user->uid;
+	echo "	<div class='user'>
+				<div class='username'>
+					<a href='index.php?page=profile&uid=$uid'>{$user->username}</a>
+				</div>
+				<div class='user-options'>
+					<input onclick='updateUsers(\"$uid-verified\", $uid, \"verified\")' name='closed' id=$uid-verified class='css-checkbox' type='checkbox' value='Yes' $verified />
+					<label for=$uid-verified class='css-label'>Verified</label>
+					<input onclick='updateUsers(\"$uid-admin\", $uid, \"admin\")' name='closed' id=$fid-closed class='css-checkbox' type='checkbox' value='Yes' $admin />
+					<label for=$fid-admin class='css-label'>Admin</label>
+				</div>
+			</div>
+			<br />";
 }
 ?>
+</div>
+<script>
+	
+</script>
