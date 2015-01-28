ff95b3dfeef75a9f2c7cf875ee8cd1a4366d24fd
Rosco Kalis <roscokalis@gmail.com>
2015-01-28 18:46:08 +0100

    Added functionality for user management. Also removed some debugging things.

--- admin/users.php
+++ admin/users.php
@@ -5,16 +5,24 @@ $res = dbquery("SELECT *
 				FROM users 
 				ORDER BY username");
 while ($user = $res->fetchObject()) {
-	$uid = $user->uid;
+	$uid      = $user->uid;
+	$verified = '';
+	$admin = '';
+	if($user->verified == 1) {
+		$verified = 'checked';
+	}
+	if($user->role == 10) {
+		$admin = 'checked';
+	}
 	echo "	<div class='user'>
 				<div class='username'>
 					<a href='index.php?page=profile&uid=$uid'>{$user->username}</a>
 				</div>
 				<div class='user-options'>
-					<input onclick='updateUsers(\"$uid-verified\", $uid, \"verified\")' name='closed' id=$uid-verified class='css-checkbox' type='checkbox' value='Yes' $verified />
+					<input onclick='updateUsers(\"$uid-verified\", $uid, \"verified\")' name='verified' id=$uid-verified class='css-checkbox' type='checkbox' value='Yes' $verified />
 					<label for=$uid-verified class='css-label'>Verified</label>
-					<input onclick='updateUsers(\"$uid-admin\", $uid, \"admin\")' name='closed' id=$fid-closed class='css-checkbox' type='checkbox' value='Yes' $admin />
-					<label for=$fid-admin class='css-label'>Admin</label>
+					<input onclick='updateUsers(\"$uid-admin\", $uid, \"admin\")' name='admin' id=$uid-admin class='css-checkbox' type='checkbox' value='Yes' $admin />
+					<label for=$uid-admin class='css-label'>Admin</label>
 				</div>
 			</div>
 			<br />";
@@ -22,5 +30,19 @@ while ($user = $res->fetchObject()) {
 ?>
 </div>
 <script>
-	
+function updateUsers(divid, user, act) {
+	var xmlhttp;
+	if (window.XMLHttpRequest) {
+		xmlhttp=new XMLHttpRequest();
+	} else {
+		xmlhttp=new ActiveXObject('Microsoft.XMLHTTP');
+	}
+	xmlhttp.onreadystatechange=function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			document.getElementById(divid).innerHTML=xmlhttp.responseText;
+		}
+	  }
+	xmlhttp.open('GET','admin/action.php?uid='+user+'&action='+act,true);
+	xmlhttp.send();
+}
 </script>
