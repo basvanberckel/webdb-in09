bd195d544c5f59afb4452856144d443668699137
Bas van Berckel <bas.vanberckel@student.uva.nl>
2015-01-29 18:09:55 +0100

    Fix ordering, all forums now sort by last post date

--- main.php
+++ main.php
@@ -11,7 +11,7 @@
     ";
     $fres = dbquery("SELECT * FROM forums 
                      WHERE parent_id=$cid AND main=1
-                     ORDER BY position;");
+                     ORDER BY lastpost_date;");
     if ($res) {
       while($frow = $fres->fetch(PDO::FETCH_ASSOC)) {
         $fid = $frow['fid'];
