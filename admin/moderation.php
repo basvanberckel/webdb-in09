cfdfb64920bd7a83ac92e342ebe2fe44a976368e
Rosco Kalis <roscokalis@gmail.com>
2015-01-29 16:27:14 +0100

    Moderation now uses updateUsers, oit should work

--- admin/moderation.php
+++ admin/moderation.php
@@ -1,16 +1,32 @@
 <div class="content" id="moderation">
 
 <?php
+function updateStats($tid, $uid, $date, $newThread) {
+  $fid = getParent($tid);
+  dbquery("UPDATE threads SET posts=posts+1, lastpost_uid=:uid, 
+          lastpost_date=FROM_UNIXTIME(:date) WHERE tid=:tid;",
+          array("uid"=>$uid, "date"=>$date, "tid"=>$tid));
+  if ($newThread) {
+    $query = "UPDATE forums SET posts=posts+1, threads=threads+1 WHERE fid=:fid;";
+  } else {
+    $query = "UPDATE forums SET posts=posts+1 WHERE fid=:fid;";
+  }
+  dbquery($query, array("fid"=>$fid));
+}
 	dbconnect();
-
+	/* If a choice is made, it will be processed here */
 	if(isset($_POST['submit'])) {
 		$submit = $_POST['submit'];
 		$pid    = $_POST['pid'];
+		$tid    = $_POST['tid'];
+		$uid    = $_POST['uid'];
 		
 		if($submit == 'Approve') {
 			$res = dbquery("UPDATE posts
 							SET approved = 1
 							WHERE pid = $pid;");
+			$date = time();
+			updateStats($tid, $uid, $date, false, 1);
 		}
 		elseif($submit == 'Decline') {
 			$res = dbquery("DELETE FROM posts
@@ -19,7 +35,7 @@
 	}
 	
 	echo "These messages await your approval:";
-
+	/* Selecting all posts that aren't approved */
 	$res = dbquery("SELECT * FROM posts, users
 					WHERE approved = 0 AND posts.uid = users.uid
 					ORDER BY date;");
@@ -30,6 +46,8 @@
 		$date     = $row['date'];
 		$username = $row['username'];
 		$pid      = $row['pid'];
+		$tid      = $row['tid'];
+		$uid      = $row['uid'];
 		
 		echo " <div class='post'>
 				   <div class='post-content'>
@@ -43,6 +61,8 @@
 				</div>";
 		echo " <form method='post'>
 					<input type='hidden' name='pid' value=$pid>
+					<input type='hidden' name='tid' value=$tid>
+					<input type='hidden' name='uid' value=$uid>
 					<div class='buttons mngbtns options'>
 						<button type='submit' name='submit' id='approve' value='Approve'>
 							Approve
