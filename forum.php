870346343ce6bd98a6fc24d6073bb3257b12ecbe
Rosco Kalis <roscokalis@gmail.com>
2015-01-29 15:07:26 +0100

    Made the New Thread button invisible if the user doesn't have the rights to create it.

--- forum.php
+++ forum.php
@@ -47,8 +47,9 @@
     echo "</div>";
   }
   echo "<div class='threads'><h2>Threads</h2>";
-  echo "<a href='/?page=discussion&fid=$fid'><div class='thread newthread'><div class='thread-data'><h3>+ New Thread</h3></div></div></a>";
-
+  if(allow('forum_posting')) {
+    echo "<a href='/?page=discussion&fid=$fid'><div class='thread newthread'><div class='thread-data'><h3>+ New Thread</h3></div></div></a>";
+  }
   $res = dbquery("SELECT COUNT(*) FROM threads 
                   WHERE fid=:fid 
                   ORDER BY lastpost_date;",
