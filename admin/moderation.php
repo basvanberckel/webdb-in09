ea5dacd5907779a207b93116a27cb9bae2cba727
Rosco Kalis <roscokalis@gmail.com>
2015-01-29 16:28:10 +0100

    Deleted the function that I accidentally added in the moderation file.

--- admin/moderation.php
+++ admin/moderation.php
@@ -1,18 +1,6 @@
 <div class="content" id="moderation">
 
 <?php
-function updateStats($tid, $uid, $date, $newThread) {
-  $fid = getParent($tid);
-  dbquery("UPDATE threads SET posts=posts+1, lastpost_uid=:uid, 
-          lastpost_date=FROM_UNIXTIME(:date) WHERE tid=:tid;",
-          array("uid"=>$uid, "date"=>$date, "tid"=>$tid));
-  if ($newThread) {
-    $query = "UPDATE forums SET posts=posts+1, threads=threads+1 WHERE fid=:fid;";
-  } else {
-    $query = "UPDATE forums SET posts=posts+1 WHERE fid=:fid;";
-  }
-  dbquery($query, array("fid"=>$fid));
-}
 	dbconnect();
 	/* If a choice is made, it will be processed here */
 	if(isset($_POST['submit'])) {
