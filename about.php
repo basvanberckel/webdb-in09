2251630474be7aee89a5b6708f8b7bcc797c1b50
Jason Compier <jason.compier@student.uva.nl>
2015-01-29 17:45:23 +0100

    Finished page

--- about.php
+++ about.php
@@ -1,6 +1,19 @@
 <div id="registration">
     <fieldset>
         <legend>About</legend>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec nunc eget enim porta feugiat. Sed euismod at purus nec vestibulum. Sed varius placerat luctus. Phasellus tellus mi, maximus et neque eu, suscipit iaculis nulla. Ut eget tristique ipsum. Donec porta vitae quam vitae convallis. Proin viverra egestas purus, eu commodo mi rhoncus eu. Maecenas nec sagittis libero, et euismod..</p>
-    </fieldset>
+        <div class="about">
+        <p>At The Duke's Herald, we all share the passion to create something extraordinary. We strive to deliver world class experiences, so we demand the very best from ourselves. For us, creativity is not a skill with any one individual so much as a mindset. By exercising a flat hierarchy and nurturing an attentive design process, we listen to all the crazy ideas thrown around the office. We believe there is no better place to grow professionally, find new friends, and kick back after a hard day’s work.
+		   Founded in 2014 by six friends from the University of Amsterdam, The Duke's Herald (then IN-09) created the cult hit My Little Pony for the PC. Ten years later, the pivotal Oenka-Boenka 8500 B.C. - MMO in 2015 would change the future of online gaming and The Duke's Herald forever. Today, the studio develops Minesweeper Online. And we are just getting warmed up.</p>
+
+
+
+		</div>
+		<div class="about">
+		<p><b>Meet a few people who work a The Duke's Herald</b></p>
+		<p>Willem Vermeulen<br />Bas van Berckel<br />Casimir Geelhoed<br />Rosco Kalis<br />Amos Bastian<br />Jason Compier</p>
+        <p><b>The Duke's Herald</b></p>
+        <p>Science Park 402<br />1098 XH Amsterdam<br />The Netherlands</p>
+		</div>
+	</fieldset>
 </div>
+   
